0c8385b636dc92811524dceabc45da0ccc5af925
shinokura <shinokura93@gmail.com>
2026-01-20 00:40:48 +0900

    初期

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// メンバーリスト
+const member = [
+    { image: "./img/hari1.jpg" , name: "ラテ", type: "活発で人懐っこい" , like: "とうもろこし・ゆでたまご" },
+    { image: "./img/hari2.jpg" ,  name: "ミルク", type: "とっても賢い" , like: "小松菜・コオロギ" },
+    { image: "./img/hari3.jpg" ,  name: "チャイ", type: "ちょっと怖がり" , like: "かぼちゃ・りんご" }
+];
+
+// 開いたとき
+// ボタンを取得してイベントの発火したボタンの詳細を呼び出す
+const openBtns = document.querySelectorAll("button[data-index]");
+openBtns.forEach(btn => {
+    btn.addEventListener('click',(e)=> {
+        const index = e.currentTarget.dataset.index;
+        const data = member[index];
+        console.log(data);
+
+        document.getElementById("modal-image").src = data.image;
+        document.getElementById("modal-name").textContent = data.name;
+        document.getElementById("modal-type").textContent = data.type;
+        document.getElementById("modal-like").textContent = data.like;
+        
+        dialog.showModal();
+    });
+});
+
+
+// 閉じるとき
+const closeBtn = document.getElementById("close-modal-btn");
+closeBtn.addEventListener('click',()=> {
+    dialog.close();
+});
+
+const dialog = document.querySelector("dialog");
